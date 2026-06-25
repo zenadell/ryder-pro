73fc7e5f60a9f89c0908184cbe56b89e1654d263
MAC <mac@Powers-MacBook-Pro.local>
2026-06-25 03:42:38 +0100

    Cleanup scratch scripts

--- test_ddgs.py
+++
@@ -1,6 +0,0 @@
-from duckduckgo_search import DDGS
-
-with DDGS() as ddgs:
-    results = ddgs.images("site:unsplash.com porsche 911", max_results=3)
-    for r in results:
-        print(r['image'])
